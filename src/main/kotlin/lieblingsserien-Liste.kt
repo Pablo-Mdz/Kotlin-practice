4d6042d97cede0eda072c8590870e54f02d47b71
Pablo-Mdz <pablocigoy@gmail.com>
2023-10-31 17:08:54 +0100

    liste 2

---
+++ src/main/kotlin/lieblingsserien-Liste.kt
@@ -0,0 +1,17 @@
+//Challenge: Lieblingsserien-Liste
+//Schreibe ein Kotlin-Programm, das die Benutzer auffordert, ihre Lieblingsserien einzugeben.
+//
+//Verwende dazu die Funktion readLine().
+//Die eingegebenen Serien sollen jeweils in einer MutableList<String> namens favoriteShows gespeichert werden.
+//
+//
+//Nachdem die Benutzer ihre Lieblingsserien eingegeben haben, füge weitere Operationen hinzu, um die favoriteShows-Liste zu manipulieren. Hier sind einige Vorschläge:
+//
+//- Sortiere die Liste alphabetisch.
+//- Entferne doppelte Einträge aus der Liste.
+//- Überprüfe, ob eine bestimmte Serie in der Liste enthalten ist.
+//- Füge eine neue Serie am Ende der Liste hinzu.
+//- Entferne eine bestimmte Serie aus der Liste.
+//
+//
+//Deine Aufgabe ist es, den Code für dieses Programm zu schreiben und sicherzustellen, dass die eingegebenen Lieblingsserien korrekt in der favoriteShows-Liste gespeichert werden. Füge dann die gewünschten Operationen hinzu, um die Liste entsprechend zu manipulieren.
