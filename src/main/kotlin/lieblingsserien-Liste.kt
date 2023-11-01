c9c58cf5b57b15d61daeedea37969f331a714596
Pablo-Mdz <pablocigoy@gmail.com>
2023-11-01 10:56:19 +0100

    liblings series app

--- src/main/kotlin/lieblingsserien-Liste.kt
+++ src/main/kotlin/lieblingsserien-Liste.kt
@@ -15,3 +15,23 @@
 //
 //
 //Deine Aufgabe ist es, den Code für dieses Programm zu schreiben und sicherzustellen, dass die eingegebenen Lieblingsserien korrekt in der favoriteShows-Liste gespeichert werden. Füge dann die gewünschten Operationen hinzu, um die Liste entsprechend zu manipulieren.
+fun main() {
+    println("bitte gebe Sie ihre 3 Liblingsseries: ")
+    var serie1 : String = readln()
+    var serie2 : String = readln()
+    var serie3 : String = readln()
+    var liblingsFilmList : MutableList<String> = mutableListOf()
+    liblingsFilmList.add(serie1)
+    liblingsFilmList.add(serie2)
+    liblingsFilmList.add(serie3)
+
+    liblingsFilmList.sort()
+    liblingsFilmList.distinct()
+    liblingsFilmList.contains(serie1)
+    liblingsFilmList.add("Game of Thrones")
+    liblingsFilmList.removeAt(1)
+
+
+
+    println(liblingsFilmList)
+}
