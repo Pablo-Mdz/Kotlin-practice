d15dfc1e3cbb479977409a31660229d7b14b77f6
Pablo-Mdz <pablocigoy@gmail.com>
2023-11-02 10:06:34 +0100

    telefonbuch fertig

---
+++ src/main/kotlin/telkefonbuch.kt
@@ -0,0 +1,16 @@
+//Challenge: Telefonbuch
+//Erstelle eine Map, die als Telefonbuch fungiert. Füge vier Einträge hinzu, wobei der Name einer Person der Schlüssel ist und die Telefonnummer der Wert. Gib anschließend die Telefonnummer einer bestimmten Person aus.
+//
+//
+
+fun main() {
+
+    val telefonNummers: Map<String, Int> = mapOf(
+        "Marco" to 1398564,
+        "Guillerme" to 34905921,
+        "Laura" to 1984322,
+        "Sandra" to 1444832
+    )
+    println("Die TelefonNummer von Guillerme ist: ${telefonNummers.get("Guillerme") } ")
+
+}
